b16390e4f58d3a82edfe338323791ae62beebcf7
nellson.maia@gmail.com <nellson.maia@gmail.com>
2019-07-23 14:21:05 +0100

    SMS Gateway

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/sms_gateway',function(req, res, next) {
+  res.render('index', { title: req });
+});
+
+router.post('/sms_gateway',function(req, res, next) {
+
+  console.log("SMS Gateway Body", req.body);
+  console.log("SMS Gateway Body",req.headers);
+  res.render('sms_gateway', { title: req.body.toString() });
+});
+
+
 module.exports = router;
